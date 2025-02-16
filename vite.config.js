48edea51373c462bfbd8a243e60996d1febe4a0a
kunal21330 <kunal21330@iiitd.ac.in>
2025-02-16 14:57:48 +0530

    4th commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './portfolio2',
+  base: '/portfolio2/',
   server: {
     host: true, // Allows access from other devices
     port: 5173, // Keep the same port
